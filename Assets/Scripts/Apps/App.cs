188f3006fdc77e430c79a73e9d4661e74d193242
idontknowwhatusername <userilovelikee@gmail.com>
2023-07-09 16:51:32 +0500

    добавил зарядку

--- Assets/Scripts/Apps/App.cs
+++ Assets/Scripts/Apps/App.cs
@@ -1,7 +1,5 @@
-using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEngine.GraphicsBuffer;
 
 public class App : MonoBehaviour
 {
@@ -19,6 +17,7 @@ public class App : MonoBehaviour
 
     public Vector3 SpawnPoint { get => spawnPoint; }
     private Vector3 spawnPoint;
+    private PlayerCollision player ;
 
 
     private void Start()
@@ -26,16 +25,17 @@ public class App : MonoBehaviour
         spawner = FindObjectOfType<WindowSpawner>();
         objectScore = GetComponent<ObjectScore>();
         spawnPoint = transform.position;
+        player = FindObjectOfType<PlayerCollision>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Trash trash))
         {
-            trashSound.Play()
-;            objectScore?.Activate();
+            trashSound.Play();
+            objectScore?.Activate();
             OnDestroy?.Invoke();
-            FindObjectOfType<PlayerCollision>().DontGrab();
+            player.DontGrab();
             Destroy(gameObject, 0.01f);
         }
     }
